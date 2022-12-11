3bf56cf32d3105c80b18f704d04a9de9f0a961f6
connerum <98507011+connerum@users.noreply.github.com>
2022-12-11 15:13:09 -0500

    Finished

--- heapsort_skeleton.py
+++ heapsort_skeleton.py
@@ -1,27 +1,24 @@
-#!/usr/bin/python3
-
 import unittest
 import math
 
 """ A HeapSort module. For more information on how to implement the functions, refer to 
-
 + CLRS3, chapter 6: the module's organization follows the text's exposition faithfully.
 + The slides for this course "Heaps: the Owner's Manual": they contain a few useful tips about the Python implementation.
-
 """
+
 
 ########### DO NOT MODIFY ####################
 class HeapCapable(list):
     """
     A HeapCapable object is just a normal Python list, with an extra `heap_size` field. For conveniency, this type is used throughout the module for all arrays, even if they are not actually max-heaps.
-
     .. warning:: Do not modify this class.
     """
+
     def __init__(self, lst):
         """
         Initializes a new heap from a list.
         """
-        super().__init__(lst) # calling the superclass (list) constructor
+        super().__init__(lst)  # calling the superclass (list) constructor
         self.heap_size = len(self)
 
 
@@ -30,145 +27,195 @@ class HeapCapable(list):
 def left(i):
     """
     Return the left child of the given node.
-    
-    The procedure is just an arithmetical computation of the L child's index. Checking that such a child exists is left to the caller function.
 
+    The procedure is just an arithmetical computation of the L child's index. Checking that such a child exists is left to the caller function.
         .. note:: Because Python arrays are numbered from 0 to 1, the pseudo-code shown in CLRS3, chapter 6 needs to be adapted (the slides "Heaps: the Owner's manual" tell you how).
- 
+
     :param i: index of the node in the heap
     :type i: int
     :return: the index of the given node's left child
     :rtype: int
         """
-    pass
+    return 2 * i + 1
+
 
 def right(i):
     """
     Return the right child of the given node.
-
     The procedure is just an arithmetical computation of the R child's index. Checking that such a child exists is left to the caller function.
-
      .. note:: Because Python arrays are numbered from 0 to 1, the pseudo-code shown in CLRS3, chapter 6 needs to be adapted (the slides "Heaps: the Owner's manual" tell you how).
-    
+
     :param i: index of the node in the heap
     :type i: int
     :return: the index of the given node's right child
     :rtype: int
     """
-    pass
+    return 2 * i + 2
+
 
 def parent(i):
     """
     Return the parent of the given node.
-
     The procedure is just an arithmetical computation of the parent's index. Checking that such a parent exists is left to the caller function.
-    
-     .. note:: Because Python arrays are numbered from 0 to 1, the pseudo-code shown in CLRS3, chapter 6 needs to be adapted (the slides "Heaps: the Owner's manual" tell you how).
 
+     .. note:: Because Python arrays are numbered from 0 to 1, the pseudo-code shown in CLRS3, chapter 6 needs to be adapted (the slides "Heaps: the Owner's manual" tell you how).
     :param i: index of the node in the heap
     :type i: int
     :return: the index of the given node's parent
     :rtype: int
     """
-    pass
+    return (i - 1) // 2
 
-def max_heapify(A,i):
+
+def max_heapify(A, i):
     """
     Given an array A, and assuming that the subtrees rooted at A[i]'s L and R children are max-heaps, restore the max-heap property.
-
         .. note:: The procedure does not return anything: it simply modifies the array in place.
-    
+
     :param A: an array.
     :param i: the index of the  node to be floated down
     :type A: heapsort_skeleton.HeapCapable
     :type i: int
     """
-    pass
+    l = left(i)
+    r = right(i)
+
+    if l < A.heap_size and A[l] > A[i]:
+        largest = l
+    else:
+        largest = i
+    if r < A.heap_size and A[r] > A[largest]:
+        largest = r
+
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, largest)
+
 
 def build_max_heap(A):
     """ Build a max-heap, from an unsorted array. 
-        
-        .. note:: The procedure does not return anything: it modifies the array in place.
 
+        .. note:: The procedure does not return anything: it modifies the array in place.
     :param A: the array to be sorted.
     :type A: heapsort_skeleton.HeapCapable
     """
-    pass
+    A.heap_size = len(A)
+    for i in range((len(A) - 1) // 2, -1, -1):
+        max_heapify(A, i)
 
 
 def HeapSort(A):
     """ Sort the array in place.
-    
-    .. note:: The function sorts in place, and therefore does not return anything.
 
+    .. note:: The function sorts in place, and therefore does not return anything.
     :param A: an array, with values in random order - do not assume that the array is already a max-heap! The `build_max_heap` procedure needs to be run on A before the actual sorting takes place.
     :type A: heapsort_skeleton.HeapCapable
     """
-    pass
+    build_max_heap(A)
+    for i in range(len(A) - 1, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        A.heap_size -= 1
+        max_heapify(A, 0)
 
+def Insert(A, x):
+    A.append(x)
 
+def Maximum(A):
+    max = 0
+    for i in range(len(A)):
+        if A[i] > A[max]:
+            max = i
+    return A[max]
+
+def Extract_Max(A):
+    max = 0
+    for i in range(len(A)):
+        if A[i] > A[max]:
+            max = i
+    item = A[max]
+    del A[max]
+    return item
+
+def Increase_Key(A, x, k):
+    for i in range(len(A)):
+        if A[i] == x:
+            A[i] = k
 
 
 ############# DO NOT MODIFY ##############################
 class testHeapSort(unittest.TestCase):
+    def test_insert_1(self):
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
+        Insert(A, 5)
+        self.assertEqual(A[14], 5)
 
-    
+    def test_maximum_1(self):
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0, 51])
+        self.assertEqual(Maximum(A), 51)
+
+    def test_extractMaximum_1(self):
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
+        self.assertEqual(Extract_Max(A), 27)
+
+    def test_increasetkey_1(self):
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
+        Increase_Key(A, 16, 25)
+        self.assertEqual(A[3], 25)
+
     def test_left_1(self):
-        self.assertEqual( left(0), 1 )
+        self.assertEqual(left(0), 1)
 
     def test_left_2(self):
-        self.assertEqual( left(2), 5 )
+        self.assertEqual(left(2), 5)
 
     def test_left_3(self):
-        self.assertEqual( left(3), 7 )
+        self.assertEqual(left(3), 7)
 
     def test_right_1(self):
-        self.assertEqual( right(0), 2 )
+        self.assertEqual(right(0), 2)
 
     def test_right_2(self):
-        self.assertEqual( right(2), 6 )
+        self.assertEqual(right(2), 6)
 
     def test_right_3(self):
-        self.assertEqual( right(3), 8 )
+        self.assertEqual(right(3), 8)
 
     def test_parent_1(self):
-        self.assertEqual( parent(1), 0 )
-    
+        self.assertEqual(parent(1), 0)
+
     def test_parent_2(self):
-        self.assertEqual( parent(2), 0 )
-    
+        self.assertEqual(parent(2), 0)
+
     def test_parent_3(self):
-        self.assertEqual( parent(3), 1 )
-    
+        self.assertEqual(parent(3), 1)
+
     def test_parent_4(self):
-        self.assertEqual( parent(4), 1 )
+        self.assertEqual(parent(4), 1)
 
     def test_max_heapify_general_case(self):
         """ 
         CLRS3, exercise 6.2-1
         """
-        A = HeapCapable([ 27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0 ])
-        max_heapify(A,2)
-        self.assertEqual( A, [27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0])
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
+        max_heapify(A, 2)
+        self.assertEqual(A, [27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0])
 
     def test_max_heapify_untouched(self):
         """ 
         MaxHeapify() does not change the array if A[i] is larger than its two children
         """
-        A = HeapCapable([ 27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0 ])
+        A = HeapCapable([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
         max_heapify(A, 1)
-        self.assertEqual(A, [ 27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0 ])
-
+        self.assertEqual(A, [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0])
 
     def test_max_heapify_reduced_heap_size(self):
         """ 
         Max-Heapify() should always ckeck against the heap's size, not the array's length!
         """
-        A = HeapCapable([ 3, 10, 7, 9, 7, 5, 2, 8, 5, 4 ])
-        A.heap_size=7
-        max_heapify(A,0)
-        self.assertEqual( A, [ 10, 9, 7, 3, 7, 5, 2, 8, 5, 4 ])
-
+        A = HeapCapable([3, 10, 7, 9, 7, 5, 2, 8, 5, 4])
+        A.heap_size = 7
+        max_heapify(A, 0)
+        self.assertEqual(A, [10, 9, 7, 3, 7, 5, 2, 8, 5, 4])
 
     def test_buildmaxheap_unique_values(self):
         """
@@ -219,13 +266,9 @@ class testHeapSort(unittest.TestCase):
         self.assertEqual(A, [7])
 
 
-
-
-
 def main():
-        unittest.main()
+    unittest.main()
+
 
 if __name__ == '__main__':
-        main()
-
-
+    main()
